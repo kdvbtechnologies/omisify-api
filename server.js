a2a54bd5b1a4da6b3fc5fc2a3ce649bbf6ff309e
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-07 21:06:21 -0200

    m

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "https://omisify.com",
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
 app.get("/", function (req, res) {
   res.send("Omisify api success");
 });
