303679a1b69abafd955d0d8f8291580ef4c01fb5
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-10-10 23:10:20 -0200

    m

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,21 +2,13 @@ const userModel = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 
 module.exports.addUser = async (req, res) => {
-  const {
-    name,
-    partnername,
-    username,
-    shortname,
-    email,
-    dateJoin,
-    country,
-    sex,
-  } = req.body;
+  const { name, partnername, shortname, email, dateJoin, country, sex } =
+    req.body;
+
   try {
     const user = await userModel.create({
       name,
       partnername,
-      username,
       shortname,
       email,
       dateJoin,
